4a355f65b1b6d5a1a7f4eebb9adc74e51c145517
puppe1990 <matheus.puppe90@hotmail.com>
2022-09-19 13:24:57 -0300

    insert quantity

--- app/models/bling_service.rb
+++ app/models/bling_service.rb
@@ -3,16 +3,17 @@ class BlingService
 
   def self.get_products
     @products = []
+    @stock_quantity = 0
     for count in (1..4) do
       @all_products = HTTParty.get("https://bling.com.br/Api/v2/produtos/page=#{count}/json/?apikey=#{@apikey}&estoque=S",
                                    headers: { content: 'application/json' })
       @all_products['retorno']['produtos'].each do |product|
         if product['produto']['estoqueAtual'] < 1000 && product['produto']['estoqueAtual'] > 900
           @products << { "codigo": product['produto']['codigo'], "descricao": product['produto']['descricao'],
-                         "estoqueAtual": product['produto']['estoqueAtual'] }
+                         "estoqueAtual": product['produto']['estoqueAtual'] - 1000 }
         end
       end
     end
-    @products
+    @products.sort_by{|p| p[:estoqueAtual]}
   end
 end
