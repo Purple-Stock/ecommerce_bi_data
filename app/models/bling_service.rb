c997e5eada47863c4a4f9e3abdc0bcc020114a7c
puppe1990 <matheus.puppe90@hotmail.com>
2022-09-19 14:11:29 -0300

    remove virtual stock products

--- app/models/bling_service.rb
+++ app/models/bling_service.rb
@@ -8,12 +8,12 @@ class BlingService
       @all_products = HTTParty.get("https://bling.com.br/Api/v2/produtos/page=#{count}/json/?apikey=#{@apikey}&estoque=S",
                                    headers: { content: 'application/json' })
       @all_products['retorno']['produtos'].each do |product|
-        if product['produto']['estoqueAtual'] < 1000 && product['produto']['estoqueAtual'] > 800
+        if product['produto']['estoqueAtual'] < 1000 && product['produto']['estoqueAtual'] > 800 && product['produto']['estrutura'].nil?
           @products << { "codigo": product['produto']['codigo'], "descricao": product['produto']['descricao'],
                          "estoqueAtual": product['produto']['estoqueAtual'] - 1000 }
         end
       end
     end
-    @products.sort_by{|p| p[:estoqueAtual]}
+    @products.sort_by { |p| p[:estoqueAtual] }
   end
 end
