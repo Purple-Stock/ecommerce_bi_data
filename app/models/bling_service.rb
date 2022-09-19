3bf3f39e36d07168a5051c9d9d5067b6d11fca5f
puppe1990 <matheus.puppe90@hotmail.com>
2022-09-19 13:33:25 -0300

    change to 800

--- app/models/bling_service.rb
+++ app/models/bling_service.rb
@@ -8,7 +8,7 @@ class BlingService
       @all_products = HTTParty.get("https://bling.com.br/Api/v2/produtos/page=#{count}/json/?apikey=#{@apikey}&estoque=S",
                                    headers: { content: 'application/json' })
       @all_products['retorno']['produtos'].each do |product|
-        if product['produto']['estoqueAtual'] < 1000 && product['produto']['estoqueAtual'] > 900
+        if product['produto']['estoqueAtual'] < 1000 && product['produto']['estoqueAtual'] > 800
           @products << { "codigo": product['produto']['codigo'], "descricao": product['produto']['descricao'],
                          "estoqueAtual": product['produto']['estoqueAtual'] - 1000 }
         end
